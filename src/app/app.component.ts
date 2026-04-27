d7560b1fb428f4cd68850b506cc391c5aa0df1b9
lizi-feld <lizif2266@gmail.com>
2026-04-27 21:24:57 +0300

    update google API to environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from "@angular/router";
 import { OnInit } from "@angular/core";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,27 @@ export class AppComponent implements OnInit
   
   }
    ngOnInit() {
- 
+   this.loadGoogleMapsApi();  
+  }
+
+  loadGoogleMapsApi() {
+    // בדיקה כדי לוודא שלא טענו את הסקריפט כבר (מונע שגיאות בניווט בין עמודים)
+    if (document.getElementById('google-maps-script')) {
+      return;
+    }
+    // יצירת תגית ה-script דרך קוד
+    const script = document.createElement('script');
+    script.id = 'google-maps-script';
+    
+    // כאן אנחנו מכניסים את המפתח מתוך ה-environment!
+    // שימי לב שהשתמשתי בגרשיים אלכסוניים (Backticks) כדי לאפשר שילוב משתנים
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}&libraries=places`;
+    
+    script.async = true;
+    script.defer = true;
+    
+    // הוספת הסקריפט לראש העמוד (head)
+    document.head.appendChild(script);
   }
 }
 
